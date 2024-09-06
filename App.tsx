306b63f9129f074e7cd385b4035e986ed005c459
Mahavir-Niranjani <niranjanimahavir@gmail.com>
2024-09-06 11:20:50 +0530

    Add only 1 statement

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ function App(): React.JSX.Element {
     <View>
       <Text style={{ fontSize: 30 }}>Hello world...</Text>
       <Text style={{ fontSize: 30 }}>React Native</Text>
+      <Text style={{ fontSize: 30 }}>This is My 1st Project</Text>
     </View>
   );
 };
