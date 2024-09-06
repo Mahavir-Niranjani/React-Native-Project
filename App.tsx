23c126d09485a1ab34ae2986afc1e51cb6c61556
Mahavir-Niranjani <niranjanimahavir@gmail.com>
2024-09-06 11:40:02 +0530

    Added press button

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   Text,
   View,
+  Button,
 } from 'react-native';
 
 function App(): React.JSX.Element {
@@ -10,6 +11,7 @@ function App(): React.JSX.Element {
       <Text style={{ fontSize: 30 }}>Hello world...</Text>
       <Text style={{ fontSize: 30 }}>React Native</Text>
       <Text style={{ fontSize: 30 }}>This is My 1st Project</Text>
+      <Button title='Press here...'></Button>
     </View>
   );
 };
